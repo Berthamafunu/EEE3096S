62270aa519786a7f3cf5e3fbd5428cd06e0b1b6a
Berthamafunu <53082975+Berthamafunu@users.noreply.github.com>
2019-08-03 17:57:26 +0200

    Update prac1.py
    finishing the code

--- Prac1/prac1.py
+++ Prac1/prac1.py
@@ -11,96 +11,126 @@ Date: <29/07/2019>
 
 import RPi.GPIO as GPIO  #importing GPIO pins
 
+
 from time import sleep   # importing  the sleep time command
+
 
 GPIO.setmode(GPIO.BOARD) #importing itertools
 
-List1 = [29, 31] #list of inputs that is the button1 and 2
 
-List2 = [11, 13, 15]    #list of the outputs that is the leds 1,2 and 3
+List1 = [16, 32] #list of inputs that is the button1 and 2
+
+
+List2 = [22, 36, 18]    #list of the outputs that is the leds 1,2 and 3
+
 
 listflow =list(product([0,1], repeat=3)) #outputs of the 3 bit counter
 
 
+
 def main():
-	print("Executing Program")
+	
+	
+	print("Executing the program")
+	
 	
 	counter = 0 #simple counter variable to remember progression in bit counter 
 	
-	sleep(1)  #setting sleeping time to 1second
+	sleep(1.5)  #setting sleeping time to 1,5seconds
+	
 	
 	button1 = GPIO.gpio_function(31)  #initialising port 31 for button1
 	
+	
 	print(button1)
+	
 	
 	button2 = GPIO.gpio_function(33)  #initialising port 33 for button 2
 	
+	
 	print(button2)
+	
 	
 	while(True):
 		
-		print("while loop is working")
+		
+		print("while the loop is working")
 		
 		#function to make the LEDs increase by one when a button is pressed
 		
 		#make Leds increase from 0 to 1 if button1 is pressed
-		if GPIO.event_detected(29):#for button1
+		
+		
+		if GPIO.event_detected(16):#for button1
+			
 			
 			counter= counter + 1
+			
                         
 			if counter ==8:
 				
+				
 				counter=0
+				
 
-			print(sequenceList[counter])
+			print(flowList[counter])
 			
-			GPIO.output(List2, sequenceList[counter])
+			
+			GPIO.output(List2,  flowList[counter])
+			
 			
 			sleep(.1)  #sleep for 0.1 seconds
+			
 
 			print("button1 works")	
 			
+			
 			#make LEDs decrease by 1 with SW2 press
-			if GPIO.event_detected(31):
+			if GPIO.event_detected(32): #for button2
 				
-				counter-=1
+				counter = counter - 1
 				
-				if counter ==-1:
+				if counter == counter -1:
 					
 					counter=7
+					
 	
-				print(sequenceList[counter])
+				print(flowList[counter])
 				
-				GPIO.output(List2, sequenceList[counter])
+				
+				GPIO.output(List2, flowList[counter])
+				
 				
 				sleep(.3) #sleep for 0.3s
+				
 	
-				print("button2 works")
+				print( "button2 works" )
 	
-			sleep(1) #sleep for 1s
-			
+			sleep(1.5) #sleep for 1s
+
+#calling main method		
 
 if __name__ == "__main__":
 	
 	try:  
 		
-		GPIO.setmode(GPIO.BOARD)  #initialising main loop so it does not iterate
+		GPIO.setmode( GPIO.BOARD )  #initialising main loop so it does not iterate
 		
 		
-		GPIO.setup(List2, GPIO.OUT, initial=GPIO.LOW)   #GPIO set to output
+		GPIO.setup(List2,  GPIO.OUT,  initial=GPIO.LOW)   #GPIO set to output
 		
 		
-		GPIO.setup(List1, GPIO.IN) #GPIO set to input
+		GPIO.setup(List1,  GPIO.IN) #GPIO set to input
 		
 		
-		GPIO.setup(List1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+		GPIO.setup(List1,  GPIO.IN,  pull_up_down = GPIO.PUD_UP)
 		
 		
 		#add edge detection and debouncing, out of loop to avoid error 
-		GPIO.add_event_detect(29, GPIO.RISING, bouncetime=200)#applying edge of detection on button1
+		GPIO.add_event_detect(16,  GPIO.RISING,  bouncetime = 250)#applying edge of detection on button1
 		
 		
-		GPIO.add_event_detect(31, GPIO.RISING, bouncetime=200)#applying edge of detection on button2	
+		GPIO.add_event_detect(32,  GPIO.RISING,  bouncetime = 250)#applying edge of detection on button2	
 		
     
 		while True:
@@ -112,16 +142,17 @@ if __name__ == "__main__":
 				
 	except KeyboardInterrupt:  
 		
-		print("Exiting gracefully")
+		print(" Exiting gracefully " )
 		
 		
 		GPIO.cleanup()      #turning off the GPIO	
+		
 		
 	except Exception as e:
 		
 		GPIO.cleanup()
 		
-		print("an error occured")
+		print( "an error occured" )
 		
 		print(e.message)	
 		
